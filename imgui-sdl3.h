b8c5ba5283d5c7e4e937ff3fb32ba8d0cda13f36
Stjepan Obrankovic <stjepano@gmail.com>
2025-01-09 15:40:57 +0100

    CMake and sdl3 helper

---
+++ imgui-sdl3.h
@@ -0,0 +1,8 @@
+#ifndef GTECH_IMGUI_SDL3_H
+#define GTECH_IMGUI_SDL3_H
+
+#include "imgui.h"
+#include "backends/imgui_impl_sdl3.h"
+#include "backends/imgui_impl_sdlrenderer3.h"
+
+#endif //GTECH_IMGUI_SDL3_H
